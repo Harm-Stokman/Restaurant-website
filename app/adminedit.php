5e59c63b4986161ce2ac03f1ddf05c9c8015050e
Harm-Stokman <harmstok2006@gmail.com>
2026-04-13 15:59:47 +0200

    Functionaliteit bewerken toegevoegd

--- app/adminedit.php
+++ app/adminedit.php
@@ -9,7 +9,42 @@ if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"] == "true") {
     header("Location: login.php");
 }
  
+
+if (isset($_GET["id"])) {
+$nameStatement = $pdo->prepare("SELECT * FROM gerechten
+WHERE id = ?"); 
+$nameStatement->bindParam(1, $_GET["id"]);
+$nameStatement->execute();
+} else {
+$nameStatement = $pdo->prepare("SELECT * FROM gerechten
+WHERE id = ?"); 
+$nameStatement->bindParam(1, $_POST["id"]);
+$nameStatement->execute();
+}
+$result= $nameStatement->fetch();
+
+
+if (isset($_POST["opslaan"])) {
+
+$id = $result['id'];
+$naam = $_POST['editgerechtnaam'];
+$ingrediënt = $_POST['editingrediënten'];
+$prijs = $_POST['editprijs'];
+
+$saveStatement = $pdo->prepare("UPDATE gerechten 
+SET gerechtnaam =  ?, ingrediënten = ?, prijs = ?
+WHERE id =  $id");
+$saveStatement->bindParam(1, $naam);
+$saveStatement->bindParam(2, $ingrediënt);
+$saveStatement->bindParam(3, $prijs);
+$saveStatement->execute();
+header("Location: admin.php");
+
+// echo "Gerecht bewerkt, ga terug om de aanpassingen te zien.";
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,42 +59,9 @@ if (isset($_SESSION["is_logged_in"]) && $_SESSION["is_logged_in"] == "true") {
     <script src="scripts/defer.js"></script>
 </head>
 
-<?php 
-if (isset($_GET["id"])) {
-$nameStatement = $pdo->prepare("SELECT * FROM gerechten
-WHERE id = ?"); 
-$nameStatement->bindParam(1, $_GET["id"]);
-$nameStatement->execute();
-} else if (isset($_POST["opslaan"])) {
-$nameStatement = $pdo->prepare("SELECT * FROM gerechten
-WHERE gerechtnaam = ?"); 
-$nameStatement->bindParam(1, $_POST["editgerechtnaam"]);
-$nameStatement->execute();
-}
-$result= $nameStatement->fetch();
-$id = $result['id'];
-
-if (isset($_POST["opslaan"])) {
-
-$naam = $_POST['editgerechtnaam'];
-$ingrediënt = $_POST['editingrediënten'];
-$prijs = $_POST['editprijs'];
-
-$saveStatement = $pdo->prepare("UPDATE gerechten 
-SET gerechtnaam =  ?, ingrediënten = ?, prijs = ?
-WHERE id =  $id");
-$saveStatement->bindParam(1, $naam);
-$saveStatement->bindParam(2, $ingrediënt);
-$saveStatement->bindParam(3, $prijs);
-$saveStatement->execute();
-
-echo "Gerecht toegevoegd, ga terug om de aanpassingen te zien.";
-
-}
-
-?>
 <body>
     <form class="admin-form" action="adminedit.php" method="post">
+        <input class="input-field" type="hidden" name="id" placeholder="ID" value="<?php echo $result['id'] ?>">
         <label>Vul hier het gerechtnaam in.</label>
         <input class="input-field" type="text" name="editgerechtnaam" placeholder="Gerechtnaam" value="<?php echo $result['gerechtnaam'] ?>">
         <label>Vul hier de ingrediënten in.</label>
--- app/login.php
+++ app/login.php
@@ -19,25 +19,43 @@ if (isset($_POST['submit'])) {
       echo "<div class='login-box'> Verkeerde inloggegevens, probeer het opnieuw. </div>";
     }
   }
+
+
+  // if (isset($_POST['username']) && isset($_POST['password'])) {
+    
+  // $query = $pdo->query("SELECT * FROM Gebruikers
+  // WHERE Gebruikersnaam = $user");
+  // $query->execute();
+  // $userfound = $query->fetchAll();
+  //   if ($userfound['Password'] == $_POST['password']) {
+  //     $_SESSION['is_logged_in'] = true;
+  //     $_SESSION['usernameLogged'] = $_POST['username'];
+  //     header('Location: index.php');
+  //   } else {
+  //     echo 'Verkeerde inloggegevens, Probeer het opnieuw.';
+  //   }
+  // }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body class="login-body">
   <div class="login-box">
     <form class="login-form" action="login.php" method="post">
-      <div class="site-logo">Sera <span>Ristorante</span></div>  
+      <div class="site-logo">Sera <span>Ristorante</span></div>
       <input class="input-field" type="text" name="username" placeholder="Gebruikersnaam">
       <input class="input-field" type="password" name="password" placeholder="Wachtwoord">
       <div class="button-field">
-      <a href="index.php"> <site-button>Terug</site-button> </a>
-      <input type="submit" name="submit" value="Login">
+        <a href="index.php"> <site-button>Terug</site-button> </a>
+        <input type="submit" name="submit" value="Login">
       </div>
     </form>
   </div>
